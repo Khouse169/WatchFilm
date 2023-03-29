d2dc8f2d1015f9e3dbf28cf6ec838ce380d5a64f
Nguyen Khai Hoan (K16_HL) <hoannkhe161601@fpt.edu.vn>
2023-03-29 22:15:28 +0700

    Add user setting

---
+++ WatchFilm/Controllers/UserSettingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WatchFilm.Controllers
+{
+    public class UserSettingController : Controller
+    {
+        public IActionResult ChangeInfor()
+        {
+            return View();
+        }
+    }
+}
